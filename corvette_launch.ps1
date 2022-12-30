001397af65e11103bbc990c47d169a067db11948
Masahiko Inoue <54964121+spearmin10@users.noreply.github.com>
2022-12-30 15:58:07 +0900

    Add files via upload

--- corvette_launch.ps1
+++ corvette_launch.ps1
@@ -1,10 +1,15 @@
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+try {
+    Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 
-$home_dir = [IO.Path]::GetFullPath((Join-Path ([IO.Path]::GetTempPath()) ".\corvette"))
-New-Item -ItemType Directory -Force -Path $home_dir > $null
+    $home_dir = [IO.Path]::GetFullPath((Join-Path ([IO.Path]::GetTempPath()) ".\corvette"))
+    New-Item -ItemType Directory -Force -Path $home_dir > $null
 
-$path = Join-Path $home_dir "corvette.ps1"
-$cli = New-Object Net.WebClient
-$cli.DownloadFile("https://raw.githubusercontent.com/spearmin10/corvette/main/corvette.ps1", $path)
+    $path = Join-Path $home_dir "corvette.ps1"
+    $cli = New-Object Net.WebClient
+    $cli.DownloadFile("https://raw.githubusercontent.com/spearmin10/corvette/main/corvette.ps1", $path)
 
-. $path
+    . $path
+} catch {
+    Write-Host $_
+    Read-Host "Hit any keys to exit"
+}
