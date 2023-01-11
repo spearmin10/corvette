2b38d00e3e2aef8af19a9a5f8638083aabf927de
Masahiko Inoue <54964121+spearmin10@users.noreply.github.com>
2023-01-11 15:51:01 +0900

    Add files via upload

---
+++ corvette-dlrun.bat
@@ -0,0 +1,7 @@
+set CORVETTE_SAVE_AS=%TEMP%\corvette.ps1
+set CORVETTE_URL=https://github.com/spearmin10/corvette/blob/main/corvette.ps1?raw=true
+
+curl -Lo "%CORVETTE_SAVE_AS%" "%CORVETTE_URL%"
+powershell -ExecutionPolicy ByPass "%CORVETTE_SAVE_AS%"
+pause
+
