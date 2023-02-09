3bace58a5ffae74d7043c5c88f1c5b2a355c71c5
Masahiko Inoue <54964121+spearmin10@users.noreply.github.com>
2023-02-09 09:42:48 +0900

    Add files via upload

--- corvette-mini.ps1
+++ corvette-mini.ps1
@@ -8,16 +8,19 @@ class Menu {
     hidden [bool]LaunchCommand($cmd) {
         switch ($cmd) {
             "1" {
-                Start-Process -FilePath "cmd.exe" -WorkingDirectory $this.home_dir
+                Start-Process -FilePath "explorer.exe" -ArgumentList @($this.home_dir)
             }
             "2" {
-                Start-Process -FilePath "powershell.exe" -WorkingDirectory $this.home_dir
+                Start-Process -FilePath "cmd.exe" -WorkingDirectory $this.home_dir
             }
             "3" {
+                Start-Process -FilePath "powershell.exe" -WorkingDirectory $this.home_dir
+            }
+            "4" {
                 $args = @("/k cd /d `"$($this.home_dir)`"")
                 Start-Process -FilePath "cmd.exe" -verb runas -ArgumentList $args
             }
-            "4" {
+            "5" {
                 $args = @("-NoExit", "-Command", "cd `"" + $this.home_dir + "`"")
                 Start-Process -FilePath "powershell.exe" -verb runas -ArgumentList $args
             }
@@ -32,10 +35,11 @@ class Menu {
         Write-Host "Corvette"
         while ($true) {
             Write-Host "************************************"
-            Write-Host " 1) Create a new command shell"
-            Write-Host " 2) Create a new powershell"
-            Write-Host " 3) Create a new command shell (Run as administrator)"
-            Write-Host " 4) Create a new powershell (Run as administrator)"
+            Write-Host " 1) Open an explorer"
+            Write-Host " 2) Create a new command shell"
+            Write-Host " 3) Create a new powershell"
+            Write-Host " 4) Create a new command shell (Run as administrator)"
+            Write-Host " 5) Create a new powershell (Run as administrator)"
 
             while (!$this.LaunchCommand((Read-Host "Please choose a menu item to run"))) {}
         }
