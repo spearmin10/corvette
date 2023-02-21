9f8af7061a94bd3d71c1f9cf8b6dc15223e742cc
Masahiko Inoue <54964121+spearmin10@users.noreply.github.com>
2023-02-21 10:08:24 +0900

    Add files via upload

---
+++ rename-office-files-jp.bat
@@ -0,0 +1,9 @@
+rename "corvette wmi.doc" "ご案内 wmi.doc"
+rename "corvette.doc" "ご案内.doc"
+rename "corvette wmi.xls" "申込書 wmi.xls"
+rename "corvette.xls" "申込書.xls"
+rename "corvette-mini wmi (offline).doc" "OFF会参加申請 wmi.doc"
+rename "corvette-mini (offline).doc" "OFF会参加申請.doc"
+rename "corvette-mini wmi (offline).xls" "OFF会参加登録 wmi.xls"
+rename "corvette-mini (offline).xls" "OFF会参加登録.xls"
+pause
