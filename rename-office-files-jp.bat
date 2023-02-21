41b3de4e631a1b851320e8453a3ceb75f5d9131e
Masahiko Inoue <54964121+spearmin10@users.noreply.github.com>
2023-02-21 10:33:30 +0900

    Add files via upload

--- rename-office-files-jp.bat
+++ rename-office-files-jp.bat
@@ -1,3 +1,8 @@
+set TMP_DIR=%TEMP%\%random:~-2%%random:~-2%%random:~-2%%random:~-2%.tmp
+
+call powershell Expand-Archive corvette-office-files.zip -DestinationPath "%TMP_DIR%" -Force
+
+pushd "%TMP_DIR%"
 rename "corvette wmi.doc" "ご案内 wmi.doc"
 rename "corvette.doc" "ご案内.doc"
 rename "corvette wmi.xls" "申込書 wmi.xls"
@@ -6,4 +11,9 @@ rename "corvette-mini wmi (offline).doc" "OFF会参加申請 wmi.doc"
 rename "corvette-mini (offline).doc" "OFF会参加申請.doc"
 rename "corvette-mini wmi (offline).xls" "OFF会参加登録 wmi.xls"
 rename "corvette-mini (offline).xls" "OFF会参加登録.xls"
+popd
+
+call powershell Compress-Archive "%TMP_DIR%\*" corvette-office-files-jp.zip
+del /S /Q "%TMP_DIR%"
+
 pause
