8e01a837e9fd71949c353a3386283c0f735a7d5a
Masahiko Inoue <54964121+spearmin10@users.noreply.github.com>
2023-06-27 15:48:51 +0900

    Add files via upload

---
+++ bin/syslog-fortigate-authlogs.ps1
@@ -0,0 +1,131 @@
+Param(
+  [parameter(mandatory=$true)][string]$SyslogHost,
+  [int]$SyslogPort = 514,
+  [string]$SyslogProtocol = "UDP",
+  [int]$SyslogFacility = 16,
+  [int]$SyslogSeverity = 6,
+  [bool]$ShowLogs = $false,
+  [parameter(mandatory=$true)][string]$SourceIP,
+  [parameter(mandatory=$true)][string]$DestinationIP,
+  [string]$UserID,
+  [parameter(mandatory=$true)][string]$Domain,
+  [parameter(mandatory=$true)][string]$LogType,
+  [int]$Count = 1
+)
+
+class Syslog {
+    [int]$pri
+
+    Syslog([int]$facility, [int]$severity) {
+        $severity = [Math]::Min($severity, 7)
+        $facility = [Math]::Min($facility, 124)
+        $this.pri = [string](($facility * 8) + $severity)
+    }
+
+    [void]Send([byte[]]$log) {
+        
+    }
+
+    [string]Build([string]$message) {
+        return "<" + $this.pri +">1 " + $(Get-Date $(Get-Date).ToUniversalTime() -Format "yyyy-MM-ddTHH:mm:ssK") + " - - - - - " + $message
+    }
+}
+
+class UdpSyslog : Syslog {
+    [System.Net.Sockets.UdpClient]$socket
+    
+    UdpSyslog([string]$sylog_host, [int]$syslog_port, [int]$facility, [int]$severity) : base($facility, $severity) {
+        $this.socket = New-Object System.Net.Sockets.UdpClient($sylog_host, $syslog_port)
+        $this.socket.DontFragment = $true
+    }
+
+    [void]Send([string]$log) {
+        $payload = [System.Text.Encoding]::UTF8.GetBytes($log)
+        [void]$this.socket.Send($payload, $payload.Length)
+    }
+
+    [void]Close() {
+        $this.socket.Close()
+    }
+}
+
+class TcpSyslog : Syslog {
+    [System.Net.Sockets.TcpClient]$socket
+    [System.Net.Sockets.NetworkStream]$stream
+    
+    TcpSyslog([string]$sylog_host, [int]$syslog_port, [int]$facility, [int]$severity) : base($facility, $severity) {
+        $this.socket = New-Object System.Net.Sockets.TcpClient
+        $this.socket.SendTimeout = 10 * 1000
+        $this.socket.Connect($sylog_host, $syslog_port)
+        $this.stream = $this.socket.GetStream()
+    }
+
+    [void]Send([string]$log) {
+        $payload = [System.Text.Encoding]::UTF8.GetBytes($log + "`r`n")
+        [void]$this.stream.Write($payload, 0, $payload.Length)
+    }
+
+    [void]Close() {
+        $this.socket.Close()
+    }
+}
+
+class Main {
+    [Syslog]$syslog
+    
+    Main([string]$syslog_protocol, [string]$sylog_host,
+         [int]$syslog_port, [int]$facility, [int]$severity) {
+        switch ($syslog_protocol) {
+            "UDP" {
+                $this.syslog = [UdpSyslog]::New($sylog_host, $syslog_port, $facility, $severity)
+            }
+            "TCP" {
+                $this.syslog = [TcpSyslog]::New($sylog_host, $syslog_port, $facility, $severity)
+            }
+            default {
+                throw "Unknown syslog protocol: " + $syslog_protocol
+            }
+        }
+    }
+
+    [void]Run([string]$client_ip,
+              [string]$target_ip,
+              [string]$user_id,
+              [string]$domain,
+              [string]$log_type,
+              [int]$count,
+              [bool]$verbose) {
+        1..$count | %{
+            [int]$timestamp = $(Get-Date $(Get-Date).ToUniversalTime() -UFormat "%s")
+            
+            $new_user_id = $user_id
+            if ([string]::IsNullOrEmpty($new_user_id)) {
+                $new_user_id = -Join (Get-Random -Count 8 -input a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
+            }
+            switch ($log_type) {
+                "NTLM-auth:success" {
+                    $log = @"
+CEF:0|Fortinet|Fortigate|v6.0.3|43017|event:user NTLM-auth success|7|deviceExternalId=FGT5HD0000000000 FTNTFGTlogid=0102043016 cat=event:user FTNTFGTsubtype=user FTNTFGTlevel=alert FTNTFGTvd=vdom1 FTNTFGTeventtime=$($timestamp)000000000 FTNTFGTlogdesc=NTLM authentication successful duser=$new_user_id FTNTFGTgroup=N/A FTNTFGTauthproto=https($client_ip) src=$client_ip dst=$target_ip FTNTFGTpolicyid=3 FTNTFGTadgroup=$domain act=NTLM-auth outcome=success reason=reason msg=AD group user succeeded in authentication
+"@
+                }
+                "NTLM-auth:failure" {
+                    $log = @"
+CEF:0|Fortinet|Fortigate|v6.0.3|43017|event:user NTLM-auth failure|7|deviceExternalId=FGT5HD0000000000 FTNTFGTlogid=0102043017 cat=event:user FTNTFGTsubtype=user FTNTFGTlevel=alert FTNTFGTvd=vdom1 FTNTFGTeventtime=$($timestamp)000000000 FTNTFGTlogdesc=NTLM authentication failed duser=$new_user_id FTNTFGTgroup=N/A FTNTFGTauthproto=https($client_ip) src=$client_ip dst=$target_ip FTNTFGTpolicyid=3 FTNTFGTadgroup=$domain act=NTLM-auth outcome=failure reason=reason msg=AD group user failed in authentication
+"@
+                default {
+                    throw "Unknown log type: " + $log_type
+                }
+            }
+
+            $this.syslog.Send($this.syslog.Build($log))
+            if ($verbose) {
+                Write-Host $log
+            } else {
+                Write-Host "log: "$client_ip" > "$target_ip
+            }
+        }
+    }
+}
+
+$main = [Main]::New($SyslogProtocol, $SyslogHost, $SyslogPort, $SyslogFacility, $SyslogSeverity)
+$main.Run($SourceIP, $DestinationIP, $UserID, $Domain, $LogType, $Count, $ShowLogs)
