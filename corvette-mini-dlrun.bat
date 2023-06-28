600186e84693366bf350e00186ac1fbad9976ada
Masahiko Inoue <54964121+spearmin10@users.noreply.github.com>
2023-06-28 19:30:06 +0900

    Update corvette-mini-dlrun.bat

--- corvette-mini-dlrun.bat
+++ corvette-mini-dlrun.bat
@@ -2,7 +2,7 @@
 set CORVETTE_SAVE_AS=%TEMP%\corvette-mini.ps1
 set CORVETTE_URL=https://github.com/spearmin10/corvette/blob/main/corvette-mini.ps1?raw=true
 
-curl -Lo "%CORVETTE_SAVE_AS%" "%CORVETTE_URL%" 2> NUL
+curl -Lo "%CORVETTE_SAVE_AS%" -H "Cache-Control: no-cache, no-store" "%CORVETTE_URL%" 2> NUL
 
 for /f "usebackq delims=" %%i in (`powershell -Command "[Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes([System.IO.File]::ReadAllText('%CORVETTE_SAVE_AS%')))"`) do set CORVETTE_B64=%%i
 
