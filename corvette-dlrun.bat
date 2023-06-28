b9eb5ac7292987c6bf68a9eba3c7d3685fc7144e
Masahiko Inoue <54964121+spearmin10@users.noreply.github.com>
2023-06-28 19:29:41 +0900

    Update corvette-dlrun.bat

--- corvette-dlrun.bat
+++ corvette-dlrun.bat
@@ -3,7 +3,7 @@
 set CORVETTE_SAVE_AS=%TEMP%\corvette.ps1
 set CORVETTE_URL=https://github.com/spearmin10/corvette/blob/main/corvette.ps1?raw=true
 
-curl -Lo "%CORVETTE_SAVE_AS%" "%CORVETTE_URL%" 2> NUL
+curl -Lo "%CORVETTE_SAVE_AS%" -H "Cache-Control: no-cache, no-store" "%CORVETTE_URL%" 2> NUL
 
 powershell -ExecutionPolicy ByPass "%CORVETTE_SAVE_AS%"
 pause
