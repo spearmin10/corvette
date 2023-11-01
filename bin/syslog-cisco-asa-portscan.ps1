378a639955b14b089807c0abb5b1c738581a5b5d
Masahiko Inoue <54964121+spearmin10@users.noreply.github.com>
2023-11-01 10:50:55 +0900

    Add files via upload

--- bin/syslog-cisco-asa-portscan.ps1
+++ bin/syslog-cisco-asa-portscan.ps1
@@ -4,17 +4,20 @@ Param(
   [string]$SyslogProtocol = "UDP",
   [int]$SyslogFacility = 16,
   [int]$SyslogSeverity = 6,
+  [string]$SyslogFormat = "RFC-5424",
   [bool]$ShowLogs = $false,
   [parameter(mandatory=$true)][string]$SourceIP,
   [parameter(mandatory=$true)][string]$DestinationIP
 )
 
 class Syslog {
+    [string]$format
     [int]$pri
 
-    Syslog([int]$facility, [int]$severity) {
+    Syslog([string]$format, [int]$facility, [int]$severity) {
         $severity = [Math]::Min($severity, 7)
         $facility = [Math]::Min($facility, 124)
+        $this.format = $format
         $this.pri = [string](($facility * 8) + $severity)
     }
 
@@ -22,15 +25,60 @@ class Syslog {
         
     }
 
+    [string]Build5424([string]$message, [string]$hostname, [string]$appname, [string]$procid) {
+        [string]$timestamp = $(Get-Date $(Get-Date).ToUniversalTime() -Format "yyyy-MM-ddTHH:mm:ssK")
+        if ([string]::IsNullOrEmpty($hostname)) {
+            $hostname = "-"
+        }
+        if ([string]::IsNullOrEmpty($appname)) {
+            $appname = "-"
+        }
+        if ([string]::IsNullOrEmpty($procid)) {
+            $procid = "-"
+        }
+        return "<" + $this.pri + ">1 ${timestamp} ${hostname} ${appname} ${procid} - - ${message}"
+    }
+
+    [string]Build3164([string]$message, [string]$hostname, [string]$appname, [string]$procid) {
+        [string]$payload = "<" + $this.pri + ">"
+        $payload += $(Get-Date).ToString("MMM dd HH:mm:ss", [System.Globalization.CultureInfo]::CreateSpecificCulture("en-US"))
+        
+        if (![string]::IsNullOrEmpty($hostname)) {
+            $payload += " $hostname"
+        }
+        if (![string]::IsNullOrEmpty($appname)) {
+            if (![string]::IsNullOrEmpty($procid)) {
+                $payload += " ${appname}[${procid}]:"
+            } else {
+                $payload += " ${appname}:"
+            }
+        }
+        return $payload + " " + $message
+    }
+
     [string]Build([string]$message) {
-        return "<" + $this.pri +">1 " + $(Get-Date $(Get-Date).ToUniversalTime() -Format "yyyy-MM-ddTHH:mm:ssK") + " - - - - - " + $message
+        return $this.Build($message, $null, $null, $null)
+    }
+
+    [string]Build([string]$message, [string]$hostname, [string]$appname, [string]$procid) {
+        switch ($this.format) {
+            "RFC-3164" {
+                return $this.Build3164($message, $hostname, $appname, $procid)
+            }
+            "RFC-5424" {
+                return $this.Build5424($message, $hostname, $appname, $procid)
+            }
+        }
+        throw "Unknown syslog format: " + $this.format
     }
 }
 
 class UdpSyslog : Syslog {
     [System.Net.Sockets.UdpClient]$socket
     
-    UdpSyslog([string]$sylog_host, [int]$syslog_port, [int]$facility, [int]$severity) : base($facility, $severity) {
+    UdpSyslog([string]$sylog_host, [int]$syslog_port,
+              [string]$syslog_format, [int]$syslog_facility, [int]$syslog_severity)
+        : base($syslog_format, $syslog_facility, $syslog_severity) {
         $this.socket = New-Object System.Net.Sockets.UdpClient($sylog_host, $syslog_port)
         $this.socket.DontFragment = $true
     }
@@ -49,7 +97,9 @@ class TcpSyslog : Syslog {
     [System.Net.Sockets.TcpClient]$socket
     [System.Net.Sockets.NetworkStream]$stream
     
-    TcpSyslog([string]$sylog_host, [int]$syslog_port, [int]$facility, [int]$severity) : base($facility, $severity) {
+    TcpSyslog([string]$sylog_host, [int]$syslog_port,
+              [string]$syslog_format, [int]$syslog_facility, [int]$syslog_severity)
+        : base($syslog_format, $syslog_facility, $syslog_severity) {
         $this.socket = New-Object System.Net.Sockets.TcpClient
         $this.socket.SendTimeout = 10 * 1000
         $this.socket.Connect($sylog_host, $syslog_port)
@@ -69,14 +119,14 @@ class TcpSyslog : Syslog {
 class Main {
     [Syslog]$syslog
     
-    Main([string]$syslog_protocol, [string]$sylog_host,
-         [int]$syslog_port, [int]$facility, [int]$severity) {
+    Main([string]$syslog_protocol, [string]$sylog_host, [int]$syslog_port,
+         [string]$syslog_format, [int]$syslog_facility, [int]$syslog_severity) {
         switch ($syslog_protocol) {
             "UDP" {
-                $this.syslog = [UdpSyslog]::New($sylog_host, $syslog_port, $facility, $severity)
+                $this.syslog = [UdpSyslog]::New($sylog_host, $syslog_port, $syslog_format, $syslog_facility, $syslog_severity)
             }
             "TCP" {
-                $this.syslog = [TcpSyslog]::New($sylog_host, $syslog_port, $facility, $severity)
+                $this.syslog = [TcpSyslog]::New($sylog_host, $syslog_port, $syslog_format, $syslog_facility, $syslog_severity)
             }
             default {
                 throw "Unknown syslog protocol: " + $syslog_protocol
@@ -95,11 +145,11 @@ class Main {
           if ($verbose) {
               Write-Host $log
           } else {
-              Write-Host "log: "$client_ip" > "$target_ip":"$target_port
+              Write-Host "log: ${client_ip} > ${target_ip}:${target_port}"
           }
         }
     }
 }
 
-$main = [Main]::New($SyslogProtocol, $SyslogHost, $SyslogPort, $SyslogFacility, $SyslogSeverity)
+$main = [Main]::New($SyslogProtocol, $SyslogHost, $SyslogPort, $SyslogFormat, $SyslogFacility, $SyslogSeverity)
 $main.Run($SourceIP, $DestinationIP, $ShowLogs)
