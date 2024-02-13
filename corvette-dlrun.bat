56815559c35250fcfba42e4e1a1df35cb7ec9774
Masahiko Inoue <54964121+spearmin10@users.noreply.github.com>
2024-02-13 10:53:51 +0900

    Add files via upload

--- corvette-dlrun.bat
+++ corvette-dlrun.bat
@@ -1,30 +1,36 @@
 @echo off
 
 set CORVETTE_HOME=%TEMP%\corvette
-set CORVETTE_SAVE_AS=%TEMP%\corvette.ps1
-set CORVETTE_SAVE_AS_TMP=%TEMP%\corvette.ps1.tmp
+set CORVETTE_SAVE_AS=%CORVETTE_HOME%\corvette.ps1
+set CORVETTE_SAVE_AS_TMP=%CORVETTE_HOME%\corvette.ps1.tmp
 set CORVETTE_URL=https://github.com/spearmin10/corvette/blob/main/corvette.ps1?raw=true
+
+if not exist "%CORVETTE_HOME%" (
+  mkdir "%CORVETTE_HOME%"
+  if errorlevel 1 (
+    echo Failed to create a folder: %CORVETTE_HOME%
+    exit /b 1
+  )
+)
 
 curl -Lo "%CORVETTE_SAVE_AS_TMP%" -H "Cache-Control: no-cache, no-store" "%CORVETTE_URL%" 2> NUL
 if errorlevel 1 (
   echo Failed to download corvette.ps1
 ) else (
   if exist "%CORVETTE_SAVE_AS%" (
-    if exist "%CORVETTE_HOME%" (
-      fc /b "%CORVETTE_SAVE_AS_TMP%" "%CORVETTE_SAVE_AS%" > NUL 2>&1
-      if errorlevel 1 (
-        rem Clean up the cache
-        pushd "%CORVETTE_HOME%"
-        for /F "usebackq" %%i in (`dir /B /A:D`) do (
-          rmdir /S /Q "%%i" > NUL 2>&1
-        )
-        for /F "usebackq" %%i in (`dir /B`) do (
-          if not "%%i"=="corvette.json" (
-            del /S /Q "%%i" > NUL 2>&1
-          )
-        )
-        popd
+    fc /b "%CORVETTE_SAVE_AS_TMP%" "%CORVETTE_SAVE_AS%" > NUL 2>&1
+    if errorlevel 1 (
+      rem Clean up the cache
+      pushd "%CORVETTE_HOME%"
+      for /F "usebackq" %%i in (`dir /B /A:D`) do (
+        rmdir /S /Q "%%i" > NUL 2>&1
       )
+      for /F "usebackq" %%i in (`dir /B`) do (
+        if not "%%i"=="corvette.json" (
+          del /S /Q "%%i" > NUL 2>&1
+        )
+      )
+      popd
     )
   )
   move /Y "%CORVETTE_SAVE_AS_TMP%" "%CORVETTE_SAVE_AS%" > NUL 2>&1
