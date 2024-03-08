fe1ff1220d6cc2db091b76d8fefe9f5ed0a3f111
Masahiko Inoue <54964121+spearmin10@users.noreply.github.com>
2024-03-08 17:34:18 +0900

    Add files via upload

--- corvette-launch.ps1
+++ corvette-launch.ps1
@@ -5,17 +5,17 @@ New-Item -ItemType Directory -Force -Path $home_dir > $null
 
 $path = Join-Path $home_dir "corvette.ps1"
 while ($true) {
-    try {
-        $cli = New-Object Net.WebClient
-        $bin = $cli.DownloadData("https://raw.githubusercontent.com/spearmin10/corvette/main/corvette.ps1")
-        if (![IO.File]::Exists($path) -Or ![Linq.Enumerable]::SequenceEqual([IO.File]::ReadAllBytes($path), $bin)) {
-            Get-ChildItem -Path $home_dir -Exclude @("corvette.json","corvette.ps1") | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
-            [IO.File]::WriteAllBytes($path, $bin)
-        }
-        . $path
-        break
-    } catch {
-        Write-Host $_
-        Read-Host "Hit any keys to retry"
-    }
+		try {
+				$cli = New-Object Net.WebClient
+				$bin = $cli.DownloadData("https://raw.githubusercontent.com/spearmin10/corvette/main/corvette.ps1")
+				if (![IO.File]::Exists($path) -Or ![Linq.Enumerable]::SequenceEqual([IO.File]::ReadAllBytes($path), $bin)) {
+						Get-ChildItem -Path $home_dir -Exclude @("corvette.json","corvette.ps1") | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
+						[IO.File]::WriteAllBytes($path, $bin)
+				}
+				. $path
+				break
+		} catch {
+				Write-Host $_
+				Read-Host "Hit any keys to retry"
+		}
 }
