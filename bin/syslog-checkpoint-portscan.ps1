31cdc4c722ade906f347716e9c90a343470955ff
Masahiko Inoue <54964121+spearmin10@users.noreply.github.com>
2024-08-08 12:29:40 +0900

    Add files via upload

---
+++ bin/syslog-checkpoint-portscan.ps1
@@ -0,0 +1,165 @@
+Param(
+  [parameter(mandatory=$true)][string]$SyslogHost,
+  [int]$SyslogPort = 514,
+  [string]$SyslogProtocol = "UDP",
+  [int]$SyslogFacility = 16,
+  [int]$SyslogSeverity = 6,
+  [string]$SyslogFormat = "RFC-5424",
+  [bool]$ShowLogs = $false,
+  [string]$App = "Unknown",
+  [parameter(mandatory=$true)][string]$SourceIP,
+  [parameter(mandatory=$true)][string]$DestinationIP
+)
+
+class Syslog {
+    [string]$format
+    [int]$pri
+
+    Syslog([string]$format, [int]$facility, [int]$severity) {
+        $severity = [Math]::Min($severity, 7)
+        $facility = [Math]::Min($facility, 124)
+        $this.format = $format
+        $this.pri = [string](($facility * 8) + $severity)
+    }
+
+    [void]Send([byte[]]$log) {
+        
+    }
+
+    [string]Build5424([string]$message, [string]$hostname, [string]$appname, [string]$procid) {
+        [string]$timestamp = $(Get-Date $(Get-Date).ToUniversalTime() -Format "yyyy-MM-ddTHH:mm:ssK")
+        if ([string]::IsNullOrEmpty($hostname)) {
+            $hostname = $Env:Computername
+            if ([string]::IsNullOrEmpty($hostname)) {
+                $hostname = "-"
+            }
+        }
+        if ([string]::IsNullOrEmpty($appname)) {
+            $appname = "-"
+        }
+        if ([string]::IsNullOrEmpty($procid)) {
+            $procid = "-"
+        }
+        return "<" + $this.pri + ">1 ${timestamp} ${hostname} ${appname} ${procid} - - ${message}"
+    }
+
+    [string]Build3164([string]$message, [string]$hostname, [string]$appname, [string]$procid) {
+        [string]$payload = "<" + $this.pri + ">"
+        $payload += $(Get-Date).ToString("MMM dd HH:mm:ss", [System.Globalization.CultureInfo]::CreateSpecificCulture("en-US"))
+        
+        if ([string]::IsNullOrEmpty($hostname)) {
+            $hostname = $Env:Computername
+            if ([string]::IsNullOrEmpty($hostname)) {
+                $hostname = "localhost"
+            }
+        }
+        $payload += " ${hostname}"
+        
+        if (![string]::IsNullOrEmpty($appname)) {
+            if (![string]::IsNullOrEmpty($procid)) {
+                $payload += " ${appname}[${procid}]:"
+            } else {
+                $payload += " ${appname}:"
+            }
+        }
+        return $payload + " " + $message
+    }
+
+    [string]Build([string]$message) {
+        return $this.Build($message, $null, $null, $null)
+    }
+
+    [string]Build([string]$message, [string]$hostname, [string]$appname, [string]$procid) {
+        switch ($this.format) {
+            "RFC-3164" {
+                return $this.Build3164($message, $hostname, $appname, $procid)
+            }
+            "RFC-5424" {
+                return $this.Build5424($message, $hostname, $appname, $procid)
+            }
+        }
+        throw "Unknown syslog format: " + $this.format
+    }
+}
+
+class UdpSyslog : Syslog {
+    [System.Net.Sockets.UdpClient]$socket
+    
+    UdpSyslog([string]$sylog_host, [int]$syslog_port,
+              [string]$syslog_format, [int]$syslog_facility, [int]$syslog_severity)
+        : base($syslog_format, $syslog_facility, $syslog_severity) {
+        $this.socket = New-Object System.Net.Sockets.UdpClient($sylog_host, $syslog_port)
+        $this.socket.DontFragment = $true
+    }
+
+    [void]Send([string]$log) {
+        $payload = [System.Text.Encoding]::UTF8.GetBytes($log)
+        [void]$this.socket.Send($payload, $payload.Length)
+    }
+
+    [void]Close() {
+        $this.socket.Close()
+    }
+}
+
+class TcpSyslog : Syslog {
+    [System.Net.Sockets.TcpClient]$socket
+    [System.Net.Sockets.NetworkStream]$stream
+    
+    TcpSyslog([string]$sylog_host, [int]$syslog_port,
+              [string]$syslog_format, [int]$syslog_facility, [int]$syslog_severity)
+        : base($syslog_format, $syslog_facility, $syslog_severity) {
+        $this.socket = New-Object System.Net.Sockets.TcpClient
+        $this.socket.SendTimeout = 10 * 1000
+        $this.socket.Connect($sylog_host, $syslog_port)
+        $this.stream = $this.socket.GetStream()
+    }
+
+    [void]Send([string]$log) {
+        $payload = [System.Text.Encoding]::UTF8.GetBytes($log + "`r`n")
+        [void]$this.stream.Write($payload, 0, $payload.Length)
+    }
+
+    [void]Close() {
+        $this.socket.Close()
+    }
+}
+
+class Main {
+    [Syslog]$syslog
+    
+    Main([string]$syslog_protocol, [string]$sylog_host, [int]$syslog_port,
+         [string]$syslog_format, [int]$syslog_facility, [int]$syslog_severity) {
+        switch ($syslog_protocol) {
+            "UDP" {
+                $this.syslog = [UdpSyslog]::New($sylog_host, $syslog_port, $syslog_format, $syslog_facility, $syslog_severity)
+            }
+            "TCP" {
+                $this.syslog = [TcpSyslog]::New($sylog_host, $syslog_port, $syslog_format, $syslog_facility, $syslog_severity)
+            }
+            default {
+                throw "Unknown syslog protocol: " + $syslog_protocol
+            }
+        }
+    }
+
+    [void]Run([string]$client_ip, [string]$target_ip, [string]$app, [bool]$verbose){
+        for ($target_port = 1; $target_port -lt 65536; $target_port++){
+          [int64]$timestamp = ($(Get-Date).ToUniversalTime().ToFileTime() - 116444736000000000) * 100
+		  [int]$client_port = $(Get-Random -Minimum 1025 -Maximum 65534)
+
+          $log = @"
+CEF:0|Check Point|VPN-1 & FireWall-1|Check Point|Log|$app|informational|act=accept spt=$client_port dpt=$target_port dst=$target_ip src=$client_ip app=$app proto=TCP
+"@
+          $this.syslog.Send($this.syslog.Build($log))
+          if ($verbose) {
+              Write-Host $log
+          } else {
+              Write-Host "log: ${client_ip} > ${target_ip}:${target_port}"
+          }
+        }
+    }
+}
+
+$main = [Main]::New($SyslogProtocol, $SyslogHost, $SyslogPort, $SyslogFormat, $SyslogFacility, $SyslogSeverity)
+$main.Run($SourceIP, $DestinationIP, $App, $ShowLogs)
