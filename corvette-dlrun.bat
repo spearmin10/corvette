44e2662b5cf4a548b773e258f0e2c33ebdf9e73a
Masahiko Inoue <54964121+spearmin10@users.noreply.github.com>
2025-02-27 20:27:16 +0900

    Add files via upload

--- corvette-dlrun.bat
+++ corvette-dlrun.bat
@@ -1,46 +1,4 @@
 @echo off
-
-set CORVETTE_HOME=%TEMP%\corvette
-set CORVETTE_SAVE_AS=%CORVETTE_HOME%\corvette.ps1
-set CORVETTE_SAVE_AS_TMP=%CORVETTE_HOME%\corvette.ps1.tmp
-set CORVETTE_URL=https://github.com/spearmin10/corvette/blob/main/corvette.ps1?raw=true
-
-if not exist "%CORVETTE_HOME%" (
-  mkdir "%CORVETTE_HOME%"
-  if errorlevel 1 (
-    echo Failed to create a folder: %CORVETTE_HOME%
-    exit /b 1
-  )
-)
-
-curl -Lo "%CORVETTE_SAVE_AS_TMP%" -H "Cache-Control: no-cache, no-store" "%CORVETTE_URL%" 2> NUL
-if errorlevel 1 (
-  echo Failed to download corvette.ps1
-) else (
-  if exist "%CORVETTE_SAVE_AS%" (
-    fc /b "%CORVETTE_SAVE_AS_TMP%" "%CORVETTE_SAVE_AS%" > NUL 2>&1
-    if errorlevel 1 (
-      rem Clean up the cache
-      pushd "%CORVETTE_HOME%"
-      for /F "usebackq" %%i in (`dir /B /A:D`) do (
-        rmdir /S /Q "%%i" > NUL 2>&1
-      )
-      for /F "usebackq" %%i in (`dir /B`) do (
-        if not "%%i"=="corvette.json" (
-          if not "%%i"=="corvette.ps1.tmp" (
-            del /S /Q "%%i" > NUL 2>&1
-          )
-        )
-      )
-      popd
-    )
-  )
-  move /Y "%CORVETTE_SAVE_AS_TMP%" "%CORVETTE_SAVE_AS%" > NUL 2>&1
-)
-
-if not exist "%CORVETTE_SAVE_AS%" (
-  echo corvette.ps1 doesn't exist.
-) else (
-  powershell -ExecutionPolicy ByPass "%CORVETTE_SAVE_AS%"
-)
+powershell -e JgAgACgAWwBTAGMAcgBpAHAAdABCAGwAbwBjAGsAXQA6ADoAQwByAGUAYQB0AGUAKABbAE4AZQB0AC4AVwBlAGIAQwBsAGkAZQBuAHQAXQA6ADoATgBlAHcAKAApAC4ARABvAHcAbgBsAG8AYQBkAFMAdAByAGkAbgBnACgAJwBoAHQAdABwAHMAOgAvAC8AZwBpAHQAaAB1AGIALgBjAG8AbQAvAHMAcABlAGEAcgBtAGkAbgAxADAALwBjAG8AcgB2AGUAdAB0AGUALwBiAGwAbwBiAC8AbQBhAGkAbgAvAGMAbwByAHYAZQB0AHQAZQAuAHAAcwAxAD8AcgBhAHcAPQB0AHIAdQBlACcAKQApACkA
+rem powershell -Command "& ([ScriptBlock]::Create([Net.WebClient]::New().DownloadString('https://github.com/spearmin10/corvette/blob/main/corvette.ps1?raw=true')))"
 pause
