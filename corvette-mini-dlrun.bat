5d03d27201457e2fadb99094ece4fb28b0cd2eab
Masahiko Inoue <54964121+spearmin10@users.noreply.github.com>
2025-02-27 23:21:20 +0900

    Add files via upload

--- corvette-mini-dlrun.bat
+++ corvette-mini-dlrun.bat
@@ -1,11 +1,4 @@
 @echo off
-set CORVETTE_SAVE_AS=%TEMP%\corvette-mini.ps1
-set CORVETTE_URL=https://github.com/spearmin10/corvette/blob/main/corvette-mini.ps1?raw=true
-
-curl -Lo "%CORVETTE_SAVE_AS%" -H "Cache-Control: no-cache, no-store" "%CORVETTE_URL%" 2> NUL
-
-for /f "usebackq delims=" %%i in (`powershell -Command "[Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes([System.IO.File]::ReadAllText('%CORVETTE_SAVE_AS%')))"`) do set CORVETTE_B64=%%i
-
-powershell -e "%CORVETTE_B64%"
+powershell -e JgAgACgAWwBTAGMAcgBpAHAAdABCAGwAbwBjAGsAXQA6ADoAQwByAGUAYQB0AGUAKABbAE4AZQB0AC4AVwBlAGIAQwBsAGkAZQBuAHQAXQA6ADoATgBlAHcAKAApAC4ARABvAHcAbgBsAG8AYQBkAFMAdAByAGkAbgBnACgAJwBoAHQAdABwAHMAOgAvAC8AZwBpAHQAaAB1AGIALgBjAG8AbQAvAHMAcABlAGEAcgBtAGkAbgAxADAALwBjAG8AcgB2AGUAdAB0AGUALwBiAGwAbwBiAC8AbQBhAGkAbgAvAGMAbwByAHYAZQB0AHQAZQAtAG0AaQBuAGkALgBwAHMAMQA/AHIAYQB3AD0AdAByAHUAZQAnACkAKQApAA==
+rem powershell -Command "& ([ScriptBlock]::Create([Net.WebClient]::New().DownloadString('https://github.com/spearmin10/corvette/blob/main/corvette-mini.ps1?raw=true')))"
 pause
-
