7e67b913ccc9abb5ba5dc4026c960dac47997922
Masahiko Inoue <54964121+spearmin10@users.noreply.github.com>
2025-03-09 01:14:55 +0900

    Update corvette-launch.ps1

--- corvette-launch.ps1
+++ corvette-launch.ps1
@@ -1,6 +1,3 @@
-$home_dir = [IO.Path]::GetFullPath((Join-Path ([IO.Path]::GetTempPath()) ".\corvette"))
-Get-ChildItem -Path $home_dir -Exclude @("corvette.json") | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
-
 while ($true) {
     try {
         & ([ScriptBlock]::Create([Net.WebClient]::New().DownloadString('https://github.com/spearmin10/corvette/blob/main/corvette.ps1?raw=true')))
